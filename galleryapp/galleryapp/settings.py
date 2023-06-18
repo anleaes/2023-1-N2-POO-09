497fd2a7a53cf58ef413760b6ff77c3be674e3f8
JoaoLoro <joaopedroloro@gmail.com>
2023-06-18 15:21:55 -0300

    Ambientação Finalizada

--- galleryapp/galleryapp/settings.py
+++ galleryapp/galleryapp/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'categories.apps.CategoriesConfig',
     'products.apps.ProductsConfig',
     'socialnetworks.apps.SocialnetworksConfig',
+    'clients.apps.ClientsConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
--- galleryapp/galleryapp/urls.py
+++ galleryapp/galleryapp/urls.py
@@ -18,6 +18,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pedidos/', include('orders.urls', namespace='orders')),
+    path('pedidos_itens/', include('orders.urls', namespace='orders_items')),
+    path('clientes/', include('clients.urls', namespace='clients')),
+    path('clientes_redessociais/', include('clients.urls', namespace='clients_socialnetworks')),
+    path('redessociais/', include('socialnetworks.urls', namespace='socialnetworks')),
     path('produtos/', include('products.urls', namespace='products')),
     path('categorias/', include('categories.urls', namespace='categories')),
 ]
