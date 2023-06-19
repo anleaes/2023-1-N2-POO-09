1779f337b0bdc1af02e1d55c40cfd5ebbddd6701
JoaoLoro <joaopedroloro@gmail.com>
2023-06-18 22:11:21 -0300

    Pessoas_metodos

--- galleryapp/apps/pessoas/models.py
+++ galleryapp/apps/pessoas/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class Pessoa ():
+class Pessoa (models.BaseModel):
     nome = models.CharField('Nome', max_length=50)
     sobrenome = models.CharField('Sobrenome', max_length=50)
+    cpf = models.IntegerField('Idade', max_lenght=11)
     idade = models.IntegerField('Idade')
     dataNascimento = models.DateField('Data Nascimento', auto_now=False, auto_now_add=False)
+
+    @staticmethod
+    def cadastrarpessoa (nome,cpf,idade,datanascimento):
+        pessoa = Pessoa.objects.create(nome = nome,cpf = cpf, idade = idade, datanascimento = datanascimento)
+        return pessoa
+    
+    @staticmethod
+    def consultarcpf(cpf):
+        if len(cpf) == 11:
+            return True
+        return False
+    
+    @staticmethod
+    def excluirpessoa (id):
+        pessoa = Pessoa.objects.get(pk=id)
+        pessoa.delete()
+        print("deletado com sucesso")
+
     #+cadastrarPessoa(nome, sobrenome, cpf, idade, dataNascimento)
     #+consultarCpf(cpf)
     #+excluirPessoa(id)
