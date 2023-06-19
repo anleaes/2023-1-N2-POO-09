3a89a85949ace11b9e4784d94a131a54e86ddefb
JoaoLoro <joaopedroloro@gmail.com>
2023-06-18 23:20:56 -0300

    Ingresso_models

---
+++ galleryapp/apps/ingressos/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from apps.exposicoes.models import Exposicao
+from apps.pessoas.models import Pessoa
+
+# Create your models here.
+class Ingresso (models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    exposicao = models.ForeignKey(Exposicao, on_delete=models.CASCADE)
+
+    @staticmethod
+    def cadastraringresso(pessoa, exposicao):
+        ingresso = Ingresso.objects.create(pessoa = pessoa,exposicao = exposicao)
+        return ingresso   
+    
+    @staticmethod
+    def excluiringresso(id):
+        ingresso = Ingresso.objects.get(pk=id)
+        ingresso.delete()
+        print("deletado com sucesso")
+
+
