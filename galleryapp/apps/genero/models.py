1a9cafff03e938ce2e28f862708ef9f90d096637
NFTHiKe <97326896+NFTHiKe@users.noreply.github.com>
2023-06-19 03:17:16 -0300

    generoMVP commit

--- galleryapp/apps/genero/models.py
+++ galleryapp/apps/genero/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Genero(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Genero'
+        verbose_name_plural = 'Generos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
--- galleryapp/apps/genero/views.py
+++ galleryapp/apps/genero/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Genero
+from rest_framework import viewsets
+from .serializer import GeneroSerializer
+
+# Após o comentario "# Create your views here." e crie as views "Genero".
+
+class GeneroViewSet(viewsets.ModelViewSet):
+    queryset = Genero.objects.all()
+    serializer_class = GeneroSerializer  
+    
