362f62a67463629d36d9dbe601f0291d267c802e
NFTHiKe <97326896+NFTHiKe@users.noreply.github.com>
2023-06-19 03:39:56 -0300

    primeira revisão

--- galleryapp/apps/acervo/models.py
+++ galleryapp/apps/acervo/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from categories.models import Category
+from genero.models import Genero
+
 
 # Create your models here.
 
 class Acervo(models.Model):
-    name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100)
+    name = models.CharField('Titulo', max_length=150)
+    year = models.IntegerField('Ano', max_length=4)
+    age = models.IntegerField('Faixa Etária', max_length=4)
+    description = models.TextField('Descricao', max_length=200)
+    artist = models.CharField('Artista', max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE, null=True)
+    
     
     class Meta:
         verbose_name = 'Acervo'
