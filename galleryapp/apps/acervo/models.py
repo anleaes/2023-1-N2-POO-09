d4c7dfeed3aecfb4b6924ef09b56f3eb0e9b7082
NFTHiKe <97326896+NFTHiKe@users.noreply.github.com>
2023-06-19 18:37:10 -0300

    models update

--- galleryapp/apps/acervo/models.py
+++ galleryapp/apps/acervo/models.py
@@ -8,7 +8,7 @@ from genero.models import Genero
 class Acervo(models.Model):
     name = models.CharField('Titulo', max_length=150)
     year = models.IntegerField('Ano', max_length=4)
-    age = models.IntegerField('Faixa Etária', max_length=4)
+    age = models.CharField('Classificacao', max_length=20)
     description = models.TextField('Descricao', max_length=200)
     artist = models.CharField('Artista', max_length=150)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
@@ -20,5 +20,25 @@ class Acervo(models.Model):
         verbose_name_plural = 'Acervos'
         ordering =['id']
 
+    
+    @staticmethod
+    def cadastrarAcervo (name,year,age,description,artist,category,genero):
+        Acervo = Acervo.objects.create(name = Titulo, year = Ano, age = Classificacao, description = Descricao, artist = Artista, category = Category, genero = Genero)
+        return Acervo
+    
+    @staticmethod
+    def excluirEspaco (id):
+        Acervo = Acervo.objects.get(pk=id)
+        Acervo.delete()
+        print("deletado com sucesso")
+
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = 'Acervo'
+        verbose_name_plural = 'Acervos'
+        ordering =['id']
+    
+    
+
