082eaad21177767900da028f036ff189e109f824
JoaoLoro <joaopedroloro@gmail.com>
2023-06-18 23:27:47 -0300

    Ingressos_views

--- galleryapp/apps/ingressos/views.py
+++ galleryapp/apps/ingressos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Ingresso
+from rest_framework import viewsets
+from .serializer import IngressosSerializer
 # Create your views here.
+class ExposicoesViewSet(viewsets.ModelViewSet):
+    queryset = Ingresso.objects.all()
+    serializer_class = IngressosSerializer  
